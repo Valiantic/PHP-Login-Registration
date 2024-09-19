15284c9f87e01ded4c1fec18abd173775d86afb9
StevenMadali <stevenmadali17@gmail.com>
2024-09-19 22:44:53 +0800

    login page skeletal form

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <h3>Login</h3>
+    <form>
+    
+        <label>Email</label><br/>
+        <input type="email" name="email" placeholder="Enter your Email Address..."/><br/>
+        <label>Password</label><br/>
+        <input type="password" name="pass" placeholder="Enter your Password..."/><br/>
+        <button type="submit">Login</button>
+        <a href="signup.php">Signup</a>
+        
+    </form>
+    
+</body>
+</html>
