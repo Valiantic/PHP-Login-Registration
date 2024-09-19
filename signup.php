8d281617fab5b6c4d7674a805947fa5db670150c
StevenMadali <stevenmadali17@gmail.com>
2024-09-19 22:40:42 +0800

    signup page skeletal form

--- signup.php
+++ signup.php
@@ -9,7 +9,19 @@
 
     <h3>Signup</h3>
     <form>
-     
+        <label>First Name</label><br/>
+        <input type="text" name="first_name" placeholder="Enter your First Name..."/><br/>
+        <label>Last Name</label><br/>
+        <input type="text" name="last_name" placeholder="Enter your Last Name..."/><br/>
+        <label>Email</label><br/>
+        <input type="email" name="email" placeholder="Enter your Email Address..."/><br/>
+        <label>Password</label><br/>
+        <input type="password" name="pass" placeholder="Enter your Password..."/><br/>
+        <label>Confirm Password</label><br/>
+        <input type="password" name="c_pass" placeholder="Confirm Passoword..."/><br/>
+        <button type="submit">Sign Up</button>
+        <a href="login.php">Login</a>
+        
     </form>
     
 </body>
